b5fdb88061b8c609d9c236c202eb2ee552bd4d75
SamiSternson <131320116+SamiSternson@users.noreply.github.com>
2026-04-07 22:30:07 +0000

    Did stuff

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// add javascript here
+let name=prompt("Please enter your name");
+let playbutton=document.getElementById("playBtn");
+let guessbutton=document.getElementById("guessBtn");
+let giveupbutton=document.getElementById("giveUpBtn");
+let message=document.getElementById("msg");
+let guess=NaN;
+let answer=NaN;
+let guessCount=0;
+let winCount=0;
+let scores=[];
+function Play()
+{
+let levels=document.getElementsByName('level');
+playbutton.disabled=true;
+for (let i=0; i<levels.length;i++)
+{
+    if (levels[i].checked)
+    {
+        range=parseInt(levels[i].value)
+    }
+    levels[i].disabled=true;
+}
+message.textContent="Guess a number between 1 and "+range;
+answer=Math.floor(Math.random() * range) + 1;
+guessbutton.disabled=false;
+giveupbutton.disable=false;
+guesscount=0;
+}
+playbutton.addEventListener("click", Play)
+// guessbuttom.addEventListener("click", function() {
+// guess=parseInt(document.getElementById("guess").value);
+// })
